9fd960e8eef7dabb9a9b35a690c3fcd8037b8042
SAM <11062003sam@gmail.com>
2025-01-04 11:05:58 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+import sqlite3
+
+data = requests.get("https://randomuser.me/api/?results=100&inc=gender,name,location")
+
+data = data.json()['results']
+
+featuredData = []
+
+for i in range(len(data)):
+    row = (data[i]['gender'],  data[i]['name']['first'], data[i]['location']['coordinates']['latitude'], data[i]['location']['coordinates']['longitude'])
+    featuredData.append(row)
+
+#print(featuredData)
+
+con = sqlite3.connect("fakeData.db")
+cur = con.cursor()
+
+for i in range(len(data)):
+    cur.executemany("INSERT INTO user VALUES(?, ?, ?, ?)", featuredData)
+    
+#cur.execute(f"INSERT INTO user VALUES({data[i]['gender']}, '{data[i]['name']['first']}', '{data[i]['location']['coordinates']['latitude']}', {data[i]['location']['coordinates']['longitude']})")
+
+con.commit()
+con.close()
